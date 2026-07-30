ecef0ee6470542560f9c2549d7c0edc54423e186
JDL <lindb310@gmail.com>
2026-07-30 00:56:22 -0500

    Make user info and user repos GitHub API calls async.

--- src/main/java/com/jdl/ghdata/core/service/UserService.java
+++ src/main/java/com/jdl/ghdata/core/service/UserService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @Service
@@ -22,11 +23,17 @@ public class UserService {
     }
 
     public UserReposResponseDto getUserAndUserRepos(String username) {
-        log.debug("Fetching GitHub user [username={}]", username);
-        GitHubApiUserResponseDto user = githubApiClient.getUser(username);
+        log.debug("Fetching GitHub user and user repos in parallel [username={}]", username);
 
-        log.debug("Fetching GitHub user repos [username={}]", username);
-        List<GitHubApiUserRepoResponseDto> repos = githubApiClient.getUserRepos(username);
+        // TODO: Need to be mindful of rate limiting, we should track frequency over time.
+        //  (The api call caching should help mitigate, but won't help if we get a batch of varied usernames)
+        CompletableFuture<GitHubApiUserResponseDto> userFuture =
+                CompletableFuture.supplyAsync(() -> githubApiClient.getUser(username));
+        CompletableFuture<List<GitHubApiUserRepoResponseDto>> reposFuture =
+                CompletableFuture.supplyAsync(() -> githubApiClient.getUserRepos(username));
+
+        GitHubApiUserResponseDto user = userFuture.join();
+        List<GitHubApiUserRepoResponseDto> repos = reposFuture.join();
 
         return new UserReposResponseDto(
                 user.login(),
