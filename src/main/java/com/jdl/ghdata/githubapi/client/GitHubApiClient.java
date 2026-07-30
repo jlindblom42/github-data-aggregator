998470527afab642f41c57d042d3929938318eed
JDL <lindb310@gmail.com>
2026-07-29 23:43:40 -0500

    Add caching for github api client calls.

--- src/main/java/com/jdl/ghdata/githubapi/client/GitHubApiClient.java
+++ src/main/java/com/jdl/ghdata/githubapi/client/GitHubApiClient.java
@@ -1,7 +1,9 @@
 package com.jdl.ghdata.githubapi.client;
 
+import com.jdl.ghdata.githubapi.config.GitHubApiCacheConfig;
 import com.jdl.ghdata.githubapi.dto.GitHubApiUserRepoResponseDto;
 import com.jdl.ghdata.githubapi.dto.GitHubApiUserResponseDto;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
@@ -17,6 +19,7 @@ public class GitHubApiClient {
         this.githubRestClient = githubRestClient;
     }
 
+    @Cacheable(GitHubApiCacheConfig.USER_CACHE)
     public GitHubApiUserResponseDto getUser(String username) {
         return githubRestClient.get()
                 .uri("/users/{username}", username)
@@ -24,6 +27,7 @@ public class GitHubApiClient {
                 .body(GitHubApiUserResponseDto.class);
     }
 
+    @Cacheable(GitHubApiCacheConfig.USER_REPOS_CACHE)
     public List<GitHubApiUserRepoResponseDto> getUserRepos(String username) {
         return githubRestClient.get()
                 .uri("/users/{username}/repos", username)
